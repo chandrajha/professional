5d6b0560a074f4c55c784a31580028ce5fe7c10b
Swamy Pasupuleti <SwamyPasupuleti@LiFung.com>
2018-11-05 11:16:13 +0530

    code updated regarding login functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { from } from 'rxjs';
+import { HomeComponent } from './home/home.component';
+import { RouterModule } from '@angular/router';
+import 'hammerjs';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    HttpClientModule,
+
+    BrowserModule,MatMenuModule, MatSidenavModule, BrowserAnimationsModule, MatButtonModule, MatCheckboxModule, FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '' , component : LoginComponent
+      },
+      {
+        path: 'home' , component : HomeComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  flag: boolean = false;
+  userType: String = '';
+  message:String;
+
+  userList = [
+                   {userName: 'pandey1@123', password: 'pandey1@123', type: 'admin'},
+                   {userName: 'pandey2@123', password: 'pandey2@123', type: 'admin'},
+                   {userName: 'pandey3#123', password: 'pandey3#123', type: 'normal'},
+                   {userName: 'pandey4#123', password: 'pandey4#123', type: 'normal'},
+                ];
+
+
+  constructor( private route: Router) { }
 
   ngOnInit() {
   }
+
+  searchUser(user,password):number{
+    for(let i=0; i< this.userList.length;i++){
+      if(user==this.userList[i].userName && password==this.userList[i].password){
+        return i;
+      
+      }
+    }
+
+  }
+
+  loginForm(){
+    if (this.flag == true){
+
+      this.route.navigateByUrl('/home');
+
+    }
+    else {
+      this.message = 'User Name/Password is Invalid.';
+      
+    }
+    
+
+    
+  }
+  checkuser(user,password){
+    var pos = this.searchUser(user,password)
+    
+    if( pos>=0 && user==this.userList[pos].userName){
+      this.flag = true;
+      this.userType = this.userList[pos].type;
+
+    }
+
+
+  }
+
+
 
 }
